00d6ae0d311503288e9578037cbfe144cbecb8d5
Charles Grivell <52409803+charlierg20@users.noreply.github.com>
2021-01-01 10:33:51 +1030

    Add files via upload

--- demosaas/config.php
+++ demosaas/config.php
@@ -9,19 +9,7 @@
     You are allowed to use the following code for commercial use, but you must follow the GNU General Public License v3.0.
   END ATTRIBUTION NOTICE */
   
-  $config = include('config.php');
+  return array(
+    'code' => '',
+  );
 ?>
-<style>
-  * {
-    font-family: 'Arial', 'Helvetica', 'sans-serif';
-  }
-</style>
-<h1>SaaS Demo Page</h1>
-<p>This page and the directory it is hosted in are, collectively, a demonstration SaaS used for testing of OpenBake, a project developed by CRG Media Group</p>
-
-<div style="color:red;">
-  <h2>This installation has been baked with code <?=$config['code']?></h2>
-  <h1 style="font-weight:bold;">
-    
-  </h1>
-</div>
